e63ae07ec309cda0eaa0e4c733a6eed453b952ba
Firstname Lastname <buckpile@yahoo.com>
2012-01-23 22:20:55 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buckpile::Application.config.secret_token = 'b6f1e961f203bbd4eeb87a41debb000c18d2af4621b75eb341fafdb86eccebbc6409f96a4296af734c2405f60f78a2b76de7224ad6804d8efc7a20141250788d'
