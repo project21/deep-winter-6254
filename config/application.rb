c907ef443ccb39bef0f621effe8c8f96ed2339c3
Firstname Lastname <buckpile@yahoo.com>
2012-02-16 10:51:52 -0800

    bdibdasdbo

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Buckpile
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-     config.force_ssl = false
+     config.force_ssl = true
     # Enable the asset pipeline
     config.assets.enabled = true
 
